72853ee7ffb47ade27456b4e7ed0a11e9d8723e9
Benny Mwangi <Bennymmwangi@gmail.com>
2022-03-11 17:25:34 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* This program prints the alphabet in lowercase without letters q and e
+*
+* Return: Always 0 (Successful)
+**/
+
+int main(void)
+{
+	char str[] = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+
+	putchar(str);
+	putchar('\n');
+	return (0);
+}
