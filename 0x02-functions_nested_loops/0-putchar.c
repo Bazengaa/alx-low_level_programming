bd49275525ad67da7db61a2f6db030c85c59f2c7
Benny Mwangi <Bennymmwangi@gmail.com>
2022-03-15 09:44:10 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main - Entry point
 *
@@ -9,6 +9,13 @@
 **/
 int main(void)
 {
-	printf("_putchar\n");
+	char *alpha = "_putchar";
+
+	while (*alpha)
+	{
+		_putchar(*alpha);
+		alpha++;
+	}
+	_putchar('\n');
 	return (0);
 }
