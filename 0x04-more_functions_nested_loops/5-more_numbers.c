917bcd9223fd4e612df46e858ee6f6fda5b4c338
Benny Mwangi <Bennymmwangi@gmail.com>
2022-03-17 14:03:51 +0000

    Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,18 @@
 void more_numbers(void)
 {
 	int i, j = 10;
-while (j--)
-{
-for (i = 0; i <= 14; ++i)
-{
-	if (i >= 10)
+
+	while (j--)
 	{
-		_putchar(i / 10 + '0');
+		for (i = 0; i <= 14; ++i)
+		{
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+	_putchar('\n');
 	}
-	_putchar(i % 10 + '0');
 }
-_putchar('\n');
-}
-}
+
