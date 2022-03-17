d2dbdfee5418a2bf0b03ce0e30c59fee1ac991cb
Benny Mwangi <Bennymmwangi@gmail.com>
2022-03-17 13:37:06 +0000

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_diagonal - Entry point & function name
+*
+* @n: variable name
+*
+* this function draws a diagonal line
+*
+* Return: Always 0
+**/
+void print_diagonal(int n)
+{
+	int i;
+	
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_square - Entry point and function name
+*
+* @size: variable name
+*
+* this function prints a square
+*
+* Return: void, returns nothing
+**/
+void print_square(int size)
+{
+	
