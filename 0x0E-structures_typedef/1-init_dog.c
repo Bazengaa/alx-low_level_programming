34dd15cc3204f7e87675451dcd54f3aef217dcb9
Benny Mwangi <Bennymmwangi@gmail.com>
2022-04-11 14:15:40 +0000

    Structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - intialize to data struct
+ * @d : pointing to data struct
+ * @name : pointer
+ * @age : float
+ * @owner : pointer
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints the data struct
+ *@d : pointer
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+}
